6122a4bd8ddfe94fda3a5c1f179c7246dbaa256a
danielroa98 <daniel.roa98@gmail.com>
2020-09-02 18:01:51 -0500

    Push 1st exercise

--- Ejercicio1/main.c
+++ Ejercicio1/main.c
@@ -1,1 +1,31 @@
+/*
+ *  Tarea 3
+ *  Programación avanzada
+ * 
+ *  Equipo 9
+ *  Daniel Roa
+ *  Miguel Monterrubio 
+ * 
+ */
+#include <stdio.h>
 #include <stdlib.h>
+
+typedef struct{
+    int childId;
+    int execTime;
+}theChild;
+
+typedef struct{
+    char procID;
+    theChild babyYoda;
+}Mando;
+
+int main(int argc, char const *argv[]){
+    
+    int procQ = 0;
+    int cProcQ = 0;
+
+    
+
+    return 0;
+}
