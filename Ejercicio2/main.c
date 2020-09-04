762b49fc0a8b7e386faef0f9e5179941e8fe2035
Miguel Monterrubio <monterrubio.miguel@gmail.com>
2020-09-04 00:20:31 -0500

    puts file sizes into urns

--- Ejercicio2/main.c
+++ Ejercicio2/main.c
@@ -6,42 +6,58 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-void crawler(char *path);
+void crawler(char *basePath, long int *max, long int *counter, long int *sizes, long int *mult);
+void urner(long int *sizes, long int counter, long int urns, long int urn);
 
 
 int main()
 {
-    // Directory path
-    char* path;
-    long long int urn;
+    
+    long int urn;
+    long int mult = 100;
+    long int counter = 0;
+    long int max = 0;
 
+    char* path;
     path = (char*)malloc(100 * sizeof(char)); 
     // char path[100] = "/mnt/d/Games/Emulators/ROMs";
 
+    long int* sizes;
+    sizes = (long int*)malloc(sizeof(long int) * mult);
+
     // Input path from user
-    printf("Enter path to list files: ");
+    printf("Enter path to list files: \n");
     // scanf("%s", path);
 
     strcpy(path, "/mnt/d/Games/Emulators/ROMs");
 
-    crawler(path);
+    crawler(path, &max, &counter, sizes, &mult);
 
     free(path);
 
-    printf("Enter urn size: ");
-    scanf("%lld", &urn);
+    // printf("Enter urn size: \n");
+    // scanf("%ld", &urn);
+    urn = 200000000;
+
+    urner(sizes, counter, max/urn, urn);
+
+    // for (int i = 0; i < counter; i++)
+    // {
+    //     printf("%ld\n", sizes[i]);
+    // }
 
     return 0;
 }
 
 
-void crawler(char *basePath)
+void crawler(char *basePath, long int *max, long int *counter, long int *sizes, long int *mult)
 {
     struct stat buffer;
     int status;
     
     char* nextPath;
     nextPath = (char*)malloc(100 * sizeof(char)); 
+
     struct dirent *dirent;
     DIR *dir = opendir(basePath);
 
@@ -53,7 +69,7 @@ void crawler(char *basePath)
     {
         if (strcmp(dirent->d_name, ".") != 0 && strcmp(dirent->d_name, "..") != 0)
         {
-            printf("%s\n", dirent->d_name);
+            
 
             strcpy(nextPath, basePath);
             strcat(nextPath, "/");
@@ -64,14 +80,36 @@ void crawler(char *basePath)
             //checks if entry is directory and sends crawler through it
             if (S_ISDIR(buffer.st_mode))
             {
-                printf("%s\n\n", "isdir");
-                crawler(nextPath);
+                crawler(nextPath, max, counter, sizes, mult);
             }
 
             //checks if entry is file and gets its size
             if (S_ISREG(buffer.st_mode))
             {
-                printf("%lld\n\n", buffer.st_size);
+                printf("Name: %s\nWeight: %ld\n\n", dirent->d_name, buffer.st_size);
+                if (buffer.st_size > *max)
+                {
+                    *max = buffer.st_size;
+                }
+                sizes[*counter] = buffer.st_size;
+                *counter += 1;
+                printf("Counter = %ld\nMax: %ld\n\n\n", *counter, *max);
+
+                // if (*counter >= *mult)
+                // {   
+                //     *mult = *mult * 2;
+                //     printf("mult is %ld\n", *mult);
+                //     sizes = (long int*)realloc(sizes, *mult);
+                //     if (sizes == NULL)
+                //     {
+                //         printf("ERROR\n");
+                //         return;
+                //     }
+                //     else
+                //     {
+                //         printf("REALLOC SUCCEEDED\n");
+                //     }
+                // }
             }
 
             
@@ -79,4 +117,27 @@ void crawler(char *basePath)
     }
 
     closedir(dir);
+}
+
+void urner(long int *sizes, long int counter, long int urnNum, long int urnSize)
+{
+    printf("Number of urns: %ld\n", urnNum);
+
+    int* urns;
+    urns = (int*)malloc(sizeof(int) * urnNum);
+
+    for (int i = 0; i < urnNum; i++)
+    {
+        urns[i] = 0;
+    }
+
+    for (int i = 0; i < counter; ++i)
+    {
+        urns[sizes[i]/urnSize] += 1;
+    }
+
+    for (int i = 0; i < urnNum; i++)
+    {
+        printf("%d\n", urns[i]);
+    }
 }
