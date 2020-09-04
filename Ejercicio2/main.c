5f0f66d2d7ee4c0d66d7d07200132d62c46ce3d7
Miguel Monterrubio <monterrubio.miguel@gmail.com>
2020-09-03 20:35:18 -0500

    directory crawler done

---
+++ Ejercicio2/main.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <string.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+void crawler(char *path);
+
+
+int main()
+{
+    // Directory path
+    char path[100] = "/mnt/d/Games/Emulators/ROMs";
+
+    // Input path from user
+    // printf("Enter path to list files: ");
+    // scanf("%s", path);
+
+    crawler(path);
+
+    return 0;
+}
+
+
+void crawler(char *basePath)
+{
+    struct stat buffer;
+    int status;
+    
+    char nextPath[1000];
+    struct dirent *dirent;
+    DIR *dir = opendir(basePath);
+
+    // if not directory or unable to open, exit
+    if (!dir)
+        return;
+
+    while ((dirent = readdir(dir)) != NULL)
+    {
+        if (strcmp(dirent->d_name, ".") != 0 && strcmp(dirent->d_name, "..") != 0)
+        {
+            printf("%s\n", dirent->d_name);
+
+            strcpy(nextPath, basePath);
+            strcat(nextPath, "/");
+            strcat(nextPath, dirent->d_name);
+
+            status = stat("nextPath", &buffer);
+            if (S_ISREG(buffer.st_mode))
+            {
+                printf("%s\n", "isdir");
+            }
+
+            crawler(nextPath);
+        }
+    }
+
+    closedir(dir);
+}
